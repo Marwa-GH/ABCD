aa0f9af8321452f2261b4f50279936a677d47963
Marwa-GH <84313167+Marwa-GH@users.noreply.github.com>
2021-12-13 20:31:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ const connectDB = require('./config/connectDB')
 const path = require('path')
 require('dotenv').config('./config/.env')
 const cors = require('cors')
-const authRouter = require('./Routes/Auth')
-const produitRouter = require("./Routes/ProduitRouter")
-const userRouter =require('./Routes/User')
-const orderRouter = require('./Routes/OrderRoutes')
 
 
 const app = express()
@@ -23,7 +19,9 @@ if(process.env.NODE_ENV === "production") {
     //set static folder
     //All the js and css files will be read and served from this folder
     app.use(express.static("frontend/build"))
-    //middleware
+    app.use(cors())
+
+//middleware
 app.use(express.json())
 //use Routes
 app.use('/api/auths',authRouter);
@@ -31,11 +29,13 @@ app.use('/api/products', produitRouter);
 app.use('/api/user',userRouter);
 app.use('/api/order', orderRouter)
 
+
     //index.html for all page routes
     app.get('*', (req, res) =>{
         res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
     })
 }
+
 
 const PORT= process.env.PORT || 6000;
 
